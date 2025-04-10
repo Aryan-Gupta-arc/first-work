e646d3a2b9d0ba10b050700c63cc3f1df758c87f
Aryan-Gupta-arc <aryangupta.arc@gmail.com>
2025-04-10 12:04:40 +0000

    data type

---
+++ Begineer/Seond.js
@@ -0,0 +1,7 @@
+let Name = "Aryan"
+let Age = 20.50
+
+console.log("Age")
+console.log(typeof Age)
+console.log(typeof null) // It was an object type 
+console.log(typeof undefine) /* It was undefine type */
