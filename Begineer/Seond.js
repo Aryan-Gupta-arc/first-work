9cbd35a1d67b65de93083903f3dd415345aa7f71
Aryan-Gupta-arc <aryangupta.arc@gmail.com>
2025-04-10 12:41:42 +0000

    Conversion of data types

--- Begineer/Seond.js
+++ Begineer/Seond.js
@@ -1,7 +1,19 @@
 let Name = "Aryan"
+let Aman = true
 let Age = 20.50
+let Gopi = 0
+Gopi = Boolean(Gopi)
 
+console.log(Gopi)
+console.log(typeof Gopi)
 console.log("Age")
 console.log(typeof Age)
 console.log(typeof null) // It was an object type 
 console.log(typeof undefine) /* It was undefine type */
+console.log(Aman)
+
+let convertInNumber = Number(Aman)
+let convertInnumber = Number(Name);
+
+console.log(typeof convertInnumber) // Here String Value convert in Number or you can say data type 
+console.log(convertInNumber)
